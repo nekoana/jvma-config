5cf37efbf3e5611af26b2499c43f7909b2228839
kouqurong <koqurong@outlook.com>
2024-09-12 07:50:52 +0800

    移除多余内容

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Form } from "@rjsf/material-ui";
+import {Button} from "@material-ui/core";
+import { RJSFSchema, UiSchema } from "@rjsf/utils";
+import validator from "@rjsf/validator-ajv8";
+import { FormEvent } from "react";
 
-function App() {
+const schema: RJSFSchema = {
+  type: "array",
+  items: {
+    type: "object",
+    title: "JVMA CONFIG",
+    properties: {
+      app_id: {
+        type: "string",
+        title: "App ID",
+        enum: ["jp.gmofg.app"],
+      },
+
+      bind_brand: {
+        type: "array",
+        title: "Bind Brand",
+        items: {
+          type: "object",
+          properties: {
+            brand_id: {
+              type: "integer",
+              uniqueItems: true,
+            },
+            brand_name: {
+              type: "string",
+            },
+            display_icon: {
+              type: "string",
+            },
+            display_name: {
+              type: "string",
+            },
+            business_category: {
+              type: "string",
+              enum: ["00", "10", "30", "72"],
+            },
+          },
+          additionalProperties: false,
+          required: [
+            "brand_id",
+            "brand_name",
+            "display_name",
+            "business_category",
+          ],
+        },
+      },
+    },
+    additionalProperties: false,
+    required: ["app_id", "bind_brand"],
+  },
+};
+
+const uiSchema: UiSchema = {
+  "ui:options": {
+    removable: true,
+    orderable: false,
+    addable: true,
+  },
+};
+
+const onSubmit = (data: any, event: FormEvent<any>) => {
+  const jsonString = JSON.stringify(data.formData);
+  console.log("Data submitted: ", jsonString);
+
+  // Create a Blob with the JSON data
+  const blob = new Blob([jsonString], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+
+  // Create a link element and trigger the download
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "data.json";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+};
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        schema={schema}
+        uiSchema={uiSchema}
+        validator={validator}
+        onSubmit={onSubmit}
+      >
+        <div className="Submit">
+          <Button variant="contained" type="submit">
+            Download
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 }
-
-export default App;
